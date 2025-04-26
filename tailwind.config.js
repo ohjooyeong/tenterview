fccd7cf9dc795d2f8c01a5d589d927ec8008c875
OH-JOO-YEONG <brb1111@naver.com>
2025-04-26 19:44:48 +0900

    [Chore] 폰트 추가 및 삭제 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
+    fontFamily: {
+      Regular: ["Pretendard-Regular"],
+      Bold: ["Pretendard-Bold"],
+      SemiBold: ["Pretendard-SemiBold"],
+      ExtraBold: ["Pretendard-ExtraBold"],
+      Black: ["Pretendard-Black"],
+      Thin: ["Pretendard-Thin"],
+      ExtraLight: ["Pretendard-ExtraLight"],
+      Light: ["Pretendard-Light"],
+      Medium: ["Pretendard-Medium"],
+    },
     extend: {},
   },
   plugins: [],
